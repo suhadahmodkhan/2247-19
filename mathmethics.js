fe0f5f63d991085439b1c6c2d78cb18210c45bd3
suhadahmodkhan <suhadahmodkhan04@gmail.com>
2024-09-21 00:34:43 -0700

    javascript mathmethics

---
+++ mathmethics.js
@@ -0,0 +1,14 @@
+var kolom = 5 ;
+var amarKacheAche = 100
+var bondurDorkar = 50 
+var bakiTaklo = amarKacheAche - bondurDorkar ;
+// console.log(bakiTaklo)
+var kintamParmu = amarKacheAche / kolom ;
+// console.log(kintamParmu)
+
+var komla = 100 ;
+var totalPrice = komla * 5 ;
+// console.log(totalPrice)
+var bondurDilo = 60 ;
+var akhonTotal = bakiTaklo + bondurDilo ;
+console.log(akhonTotal)
